391c91e944bd160aa84794a1ce4beb1f3f352ed0
Tanmay Joshi <tanmayjoshidun@gmail.com>
2025-09-26 14:41:55 +0530

    inventory service initial commit

---
+++ src/main/java/com/tanmay/e_commerce_application/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.e_commerce_application.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/e_commerce_application/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.e_commerce_application.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
