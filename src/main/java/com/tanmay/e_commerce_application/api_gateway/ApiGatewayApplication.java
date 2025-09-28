fbb842c4d6f9fb145c55a1cb4abad05500149fbf
Tanmay Joshi <tanmayjoshidun@gmail.com>
2025-09-28 18:16:48 +0530

    API gateway initial commit

---
+++ src/main/java/com/tanmay/e_commerce_application/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.tanmay.e_commerce_application.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
